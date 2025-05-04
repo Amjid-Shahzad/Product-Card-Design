6f6a8046ea5d2ff8492f0afce126772b3dbec172
Amjid-Shahzad <amjid.shahzad47@gmail.com>
2025-05-04 20:58:58 +0300

    Product card design in HTML CSS

---
+++ js/data.js
@@ -0,0 +1,27 @@
+let demoData = [{
+    Id: 1,
+    Name: "Amjad Shahzhad",
+    personalInfo: "Amjad shahzad is modle class person and trying has life to become a perfect and good person"
+}, {
+    Id: 1,
+    Name: "Ali",
+    personalInfo: "Ali is modle class person and trying has life to become a perfect and good person"
+}, {
+    Id: 1,
+    Name: "Naeem Abbasi",
+    personalInfo: "Naeem abbasi is modle class person and trying has life to become a perfect and good person"
+}, {
+    Id: 1,
+    Name: "Asif Abbasi",
+    personalInfo: "Asif Abbasi is modle class person and trying has life to become a perfect and good person"
+}, {
+    Id: 1,
+    Name: "Madni Khan",
+    personalInfo: "Madni Khan is modle class person and trying has life to become a perfect and good person"
+}, {
+    Id: 1,
+    Name: "Shamsher Abbasi",
+    personalInfo: "Shamsher Abbasi is modle class person and trying has life to become a perfect and good person"
+}];
+
+console.log("Hello New Code")
